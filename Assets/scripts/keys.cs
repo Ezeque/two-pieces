754e99914e87ac8d8ddcc0370af30665659c4205
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-11-29 20:49:23 -0300

    modularizada a lógica do spawn aleatório

--- Assets/scripts/keys.cs
+++ Assets/scripts/keys.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class keys : MonoBehaviour
 {
-    public Vector2 worldBottomLeft;
-    public Vector2 worldTopRight;
     // Start is called before the first frame update
     void Start()
     {
-        randomSpawn();
+        
     }
 
     // Update is called once per frame
@@ -18,26 +16,5 @@ public class keys : MonoBehaviour
         
     }
 
-    private void randomSpawn(){
-        float minX = worldBottomLeft.x;
-        float maxX = worldTopRight.x;
-        float minY = worldBottomLeft.y;
-        float maxY = worldTopRight.y;
-        bool isValidPosition = false;
-        float newY = 0;
-        float newX = 0;
-        Vector2 novaPosicao;
-
-        while(!isValidPosition){
-            newY = Random.Range(minY, maxY);
-            newX = Random.Range(minX, maxX);
-            
-            Collider2D colisorNaPosicao = Physics2D.OverlapPoint(new Vector2(newX, newY), 1 << LayerMask.NameToLayer("Island"));
-            if(colisorNaPosicao == null){
-                isValidPosition = true;
-            }
-        }
-        novaPosicao = new Vector2(newX, newY);
-        transform.position = new Vector3(novaPosicao.x, novaPosicao.y, -1);
-    }
+    
 }
